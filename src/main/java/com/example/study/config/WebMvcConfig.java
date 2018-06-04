37dfbbcce7b250d6c31c20d0bd334d0d56d526ca
lluo2010 <hongchuanluo@outlook.com>
2018-06-04 18:34:13 +0800

    add logback config

--- src/main/java/com/example/study/config/WebMvcConfig.java
+++ src/main/java/com/example/study/config/WebMvcConfig.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +21,8 @@ import java.util.List;
 /**
  * Created by louis on 2018/5/30.
  */
+@Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
